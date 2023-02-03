58e2900449b2a2a897d99c4d8a89e5b5bbcbabf8
ronald <ronald881996@gmail.com>
2023-02-03 13:35:55 +0100

    last commit

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -5,7 +5,6 @@ import { ErrorResponse, SuccessResponse } from '../types/Responses';
 import { HTTP_STATUS, RESPONSE_ERROR_MESSAGE } from '../utils/constants';
 import { CreateErrorResponse, CreateSuccessResponse } from '../utils/responses';
 
-
 export const login = async ({
 	email,
 	password,
@@ -14,7 +13,8 @@ export const login = async ({
 	password: string;
 }): Promise<SuccessResponse | ErrorResponse> => {
 	try {
-		if (!(email && password)) return CreateErrorResponse(HTTP_STATUS.BAD_REQUEST, RESPONSE_ERROR_MESSAGE.INPUTS_REQUIRED);
+		if (!(email && password))
+			return CreateErrorResponse(HTTP_STATUS.BAD_REQUEST, RESPONSE_ERROR_MESSAGE.INPUTS_REQUIRED);
 
 		const user = await User.findOne({ email }).exec();
 		if (user && (await bcrypt.compare(password, user.password))) {
