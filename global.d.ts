aefa08335be3763534a77ae789e1219f17cf993a
ronald <ronald881996@gmail.com>
2023-02-03 13:38:38 +0100

    format

--- global.d.ts
+++ global.d.ts
@@ -2,7 +2,6 @@ namespace NodeJS {
 	interface ProcessEnv {
 		JWT_TOKEN: string;
 		URI_MONGODB_PROD: string;
-		URI_MONGODB_DEV: string;
-		NODE_ENV:string
+		NODE_ENV: string;
 	}
 }
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    setupFiles: ['dotenv/config', './src/testing/setup.js'],
-    modulePathIgnorePatterns: ['./build']
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	setupFiles: ['dotenv/config', './src/testing/setup.js'],
+	modulePathIgnorePatterns: ['./build'],
 };
